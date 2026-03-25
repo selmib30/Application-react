c541c3b311af73ceebc65ca0c43113b0e8f746c1
kerker48 <kerker48230@gmail.com>
2026-03-25 14:51:12 +0100

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "http://127.0.0.1:8000",
+  withCredentials: true,
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+  },
 });
 
 export default api;
